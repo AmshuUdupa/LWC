fee65a0cc093d218bc886a2e7db1f316d4c1ed20
AmshuUdupa <udupa.amshu002@gmail.com>
2020-09-08 16:52:57 +0530

    first cimmit
    Master

---
+++ force-app/main/default/lwc/graphs/graphs.js
@@ -0,0 +1,73 @@
+import { LightningElement, track, api } from 'lwc';
+
+import resourceContainer from '@salesforce/resourceUrl/Resource';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+
+export default class Graphs extends LightningElement {
+@track error;
+steppedChart;
+@api greeting;
+@api heading;
+@api message;
+chartjsInitialized = false;
+
+/* generate the URL for the JavaScript*/
+charts = resourceContainer + '/d3-funnel.min.js';
+charts2 = resourceContainer + '/d3.v4.min.js';
+ renderedCallback() {
+    /*eslint-disable */
+    console.log("Rendered call");
+    if (this.chartjsInitialized) {
+        return;
+    }
+    this.chartjsInitialized = true;
+
+    Promise.all([
+            loadScript(this, resourceContainer + '/d3-funnel.min.js'),
+            loadScript(this, resourceContainer + '/d3.v4.min.js')
+        ]).then(() => {
+        console.log("call ne");    
+        this.generateSteppedChart();
+    })
+    .catch(error => {
+        this.error = error;
+        console.log('Error Occured ', error);
+    });
+}
+errorCallback(error, stack) {
+    this.error = error;
+    console.log('call back error this.error ', this.error);
+}
+generateSteppedChart(){
+    console.log("in stepped graph");
+    var data =    
+    [    
+        ['PROSPECT', 2500000],    
+        ['PROPOSAL', 1700000],    
+        ['NEGOTIATION', 1000000],    
+        ['DEAL', 500000]    
+    ];    
+    var options =    
+        {    
+        chart:    
+            {    
+            curve:    
+            {    
+                enabled: true    
+            }    
+        }    
+    };
+    
+    const ctx = this.template
+                .querySelector('canvas.stepped')
+                .getContext('2d');
+    console.log("ctx>>"+ctx) ;
+    //chart = new D3Funnel(ctx);
+   // console.log("chart>>>"+chart); 
+    
+    //chart.draw(data, options);    
+
+    //this.steppedChart = new window.Chart(data, options);  
+}
+
+}
