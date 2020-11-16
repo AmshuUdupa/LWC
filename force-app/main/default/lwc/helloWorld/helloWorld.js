8370ae522b8deda70ac68f497fc4d9f47699e52a
AmshuUdupa <udupa.amshu002@gmail.com>
2020-11-16 20:18:41 +0530

    11/16

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,21 +1,31 @@
 import { LightningElement, track } from 'lwc';
-import templateOne from './templateOne.html'
-import templateTwo from './templateTwo.html'
+// import templateOne from './templateOne.html'
+// import templateTwo from './templateTwo.html'
 
 
 export default class HelloWorld extends LightningElement {
     @track dynamicGreeting = 'World';
     detailsVisible = false;
     templateOne = true;
-
-    render() {
-        return this.templateOne ? this.templateOne : templateTwo;
+    firstName = '';
+    lastName = '';
+    fullName = { firstName: '', lastName: '' };
+    @track x;
+    inIt() {
+        this.x = new Date();
     }
-
-    switchTemplate(event) {
-
-        this.templateOne = this.template === true ? false : true;
+    setDate() {
+        this.x.setHours(7);
+        console.log("setter");
     }
+    // render() {
+    //     return this.templateOne ? this.templateOne : templateTwo;
+    // }
+
+    // switchTemplate(event) {
+
+    //     this.templateOne = this.template === true ? false : true;
+    // }
 
     contacts = [
         { Id: 1, name: 'Asha' },
@@ -33,5 +43,24 @@ export default class HelloWorld extends LightningElement {
         console.log("clicked");
     }
 
+    handleChange(event) {
+        const field = event.target.name;
+        if (field === "firstname") {
+            this.firstName = event.target.value;
+            this.fullName.firstName = event.target.value;
+            console.log('first name set');
+        }
+        else if (field === "lastname") {
+            this.lastName = event.target.value;
+            // this.fullName.lastName= event.target.value;
+            console.log('last name set');
+
+        }
+    }
+
+    get changeUpperCase() {
+        console.log('uppercase')
+        return `${this.firstName} ${this.lastName}`.trim().toUpperCase();
+    }
 
 }
